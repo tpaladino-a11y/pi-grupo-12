55224c6a3c7ace9d7e753ada7b546483e7bf6348
tpaladino-a11y <tpaladino@udesa.edu.ar>
2025-11-09 23:44:46 -0300

    Finalizacion pagina login

--- js/login.js
+++ js/login.js
@@ -14,3 +14,34 @@ formularioheader.addEventListener("submit", function(e) {
 
 
 /// Valido que se haya escrito algo en los campos, similar al funcionamiento del header
+let formularioLogin =  document.querySelector(".formLogin");
+let inputLoginEmail = document.querySelector(".inputLoginEmail");
+let inputLoginContra = document.querySelector(".inputLoginContra");
+
+formularioLogin.addEventListener("submit", function(e){
+    let textoEmailLogin = inputLoginEmail.value.trim();
+    let caracteresEmailLogin = textoEmailLogin.length;
+
+    let textoContraLogin = inputLoginContra.value.trim();
+    let caracteresContraLogin = textoContraLogin.length;
+
+
+    if (caracteresEmailLogin === 0 || caracteresContraLogin === 0){    e.preventDefault(); 
+        e.preventDefault(); 
+        alert("Debe de completar los campos")
+    } else if (caracteresEmailLogin === 0){
+        e.preventDefault(); 
+        alert("Debe ingresar un Email.");
+    }else if (caracteresContraLogin === 0){
+        e.preventDefault(); 
+        alert("Debe ingresar una contraseña");
+    }else if (caracteresContraLogin > 0 && caracteresContraLogin < 6){
+        alert("Debe ingresar una contraseña de al menos 6 caracteres");
+        e.preventDefault(); 
+
+    }else{
+        
+        // Guarda el email en localStorage
+        localStorage.setItem("usuarioEmail", textoEmailLogin);
+    }
+})
