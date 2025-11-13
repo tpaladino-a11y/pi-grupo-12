aaa8375d3571e383e931e37459d833c6ec85313d
facu-gif736 <martinezfacundo@udesa.edu.ar>
2025-11-13 13:59:46 -0300

    Pagina registro y search-results JS

---
+++ js/registro.js
@@ -0,0 +1,40 @@
+let formRegistro = document.querySelector(".formRegistro"); 
+let inputEmailReg = document.querySelector(".inputRegistroEmail"); 
+let inputContrasenia = document.querySelector(".inputRegistroContra"); 
+let inputRepContrasenia = document.querySelector(".inputRepiteContra"); 
+let checkboxAcepto = document.querySelector(".checkboxAcepto"); 
+
+formRegistro.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    
+    let email = inputEmailReg.value.trim();
+    let contrasenia = inputContrasenia.value;
+    let repContrasenia = inputRepContrasenia.value;
+
+    if (email.length == 0 || contrasenia.length == 0 || repContrasenia.length == 0) {
+        alert("¡Error! Debes completar todos los campos obligatorios.");
+        return; 
+    }
+    
+    if (contrasenia.length > 0 && contrasenia.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres"); 
+        return;
+    }
+    
+    if (contrasenia !== repContrasenia) {
+        alert("Las contraseñas no coinciden"); 
+        return;
+    }
+    
+    if (checkboxAcepto.checked == false) { 
+        alert("Es obligatorio aceptar los términos y condiciones.");
+        return;
+    }
+
+    localStorage.setItem("usuarioEmail", email); 
+    
+    alert("¡Registro exitoso! Redirigiendo a Login.");
+    
+    window.location.href = "login.html"; 
+    
+});
