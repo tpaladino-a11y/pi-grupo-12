2ddbf1392a733810ec564c3c2ca1891ade0d325b
facu-gif736 <martinezfacundo@udesa.edu.ar>
2025-11-14 18:08:44 -0300

    Cambio en el codigo del registro.js

--- js/registro.js
+++ js/registro.js
@@ -11,3 +11,36 @@ formularioheader.addEventListener("submit", function(e) {
         alert("Debe ingresar al menos 3 caracteres para realizar la búsqueda.");
     }
 });
+
+let formularioRegistro = document.querySelector(".formRegistro");
+
+let inputEmail = document.querySelector("#emailRegistro");
+let inputContraseña = document.querySelector("#contraseñaRegistro");
+let inputRepContraseña = document.querySelector("#repContraseñaRegistro");
+let checkboxTerminos = document.querySelector("#aceptoTerminos");
+
+formularioRegistro.addEventListener("submit", function(e){
+    let email = inputEmail.value.trim();
+    let contraseña = inputContraseña.value.trim();
+    let repContraseña = inputRepContraseña.value.trim();
+
+    let errorMensaje = "";
+
+    if (email.length === 0) {
+        errorMensaje = "EL campo email esta vacio, porfavor completarlo.";
+    }
+
+    else if (contraseña.length === 0) {
+        errorMensaje = "El campo contraseña esta vacio, porfavor completarlo.";
+    }
+
+    else if (contraseña < 6) {
+        errorMensaje = "La contraseña debe tener al menos 6 caracteres.";
+    }
+
+    else if (contraseña !== repContraseña) {
+        errorMensaje = "Las contraseñas no coinciden.";
+    }
+
+    
+})
