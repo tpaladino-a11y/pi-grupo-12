eaca5ce42fdc902fac1069ee9619d844b5ffdec5
facu-gif736 <martinezfacundo@udesa.edu.ar>
2025-11-14 17:54:16 -0300

    Cambio en el Header del registro

--- js/registro.js
+++ js/registro.js
@@ -1,40 +1,13 @@
-let formRegistro = document.querySelector(".formRegistro"); 
-let inputEmailReg = document.querySelector(".inputRegistroEmail"); 
-let inputContrasenia = document.querySelector(".inputRegistroContra"); 
-let inputRepContrasenia = document.querySelector(".inputRepiteContra"); 
-let checkboxAcepto = document.querySelector(".checkboxAcepto"); 
+/// HEADER (mismo en todas las paginas)
+let formularioheader = document.querySelector(".search");
+let inputheader = document.querySelector(".inputheader");
 
-formRegistro.addEventListener("submit", function(e) {
-    e.preventDefault(); 
-    
-    let email = inputEmailReg.value.trim(); /// el trim lo que hace es que no cuenta los espacios a la hora de contar caracteres
-    let contrasenia = inputContrasenia.value;
-    let repContrasenia = inputRepContrasenia.value;
+formularioheader.addEventListener("submit", function(e) {
+    let texto = inputheader.value.trim();  /// el trim lo que hace es que no cuenta los espacios a la hora de contar caracteres
+    let cantdecaracteres = texto.length;
 
-    if (email.length == 0 || contrasenia.length == 0 || repContrasenia.length == 0) {
-        alert("Debes completar todos los campos obligatorios.");
-        return; 
+    if (cantdecaracteres === 0 || cantdecaracteres < 3) {
+        e.preventDefault(); 
+        alert("Debe ingresar al menos 3 caracteres para realizar la búsqueda.");
     }
-    
-    if (contrasenia.length > 0 && contrasenia.length < 6) {
-        alert("La contraseña debe tener al menos 6 caracteres"); 
-        return;
-    } 
-    
-    if (contrasenia !== repContrasenia) {
-        alert("Las contraseñas no coinciden"); 
-        return;
-    }
-    
-    if (checkboxAcepto.checked == false) { 
-        alert("Es obligatorio aceptar los términos y condiciones.");
-        return;
-    }
-
-    localStorage.setItem("usuarioEmail", email); 
-    
-    alert("Redirigiendo a Login.");
-    
-    window.location.href = "login.html"; 
-    
 });
